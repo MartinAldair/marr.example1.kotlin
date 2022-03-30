564f2e56c0b4f218096d727868fc84ea1565f9b6
DESKTOP-LER948U\Martin_Aldair <m.a.rmz.ramos@outlook.com>
2022-03-30 17:26:33 -0600

    #VII@Dev&Implemented

--- src/main/kotlin/mx/com/marr/example1/kotlin/Application.kt
+++ src/main/kotlin/mx/com/marr/example1/kotlin/Application.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan(basePackages = ["io.swagger", "mx.com.marr.example1.kotlin"])
+@ComponentScan(basePackages = ["mx.com.marr.example1.kotlin", "io.swagger", "io.swagger.config"])
 class Application
 
 fun main(args: Array<String>) {
