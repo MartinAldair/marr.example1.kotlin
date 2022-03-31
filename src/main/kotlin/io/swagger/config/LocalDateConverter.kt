0206c4344d0e0d50dd83f6aeaf17996f391f9063
DESKTOP-LER948U\Martin_Aldair <m.a.rmz.ramos@outlook.com>
2022-03-30 20:05:12 -0600

    #IV@Dev&Implemented

--- src/main/kotlin/io/swagger/config/LocalDateConverter.kt
+++ src/main/kotlin/io/swagger/config/LocalDateConverter.kt
@@ -1,4 +1,20 @@
 package io.swagger.config
 
-class LocalDateConverter {
+import org.springframework.core.convert.converter.Converter;
+import org.threeten.bp.LocalDate;
+import org.threeten.bp.format.DateTimeFormatter;
+
+class LocalDateConverter:Converter<String, LocalDate> {
+
+    private var formatter: DateTimeFormatter? = null
+
+    fun LocalDateConverter(dateFormat: String?) {
+        formatter = DateTimeFormatter.ofPattern(dateFormat)
+    }
+
+    override fun convert(source: String): LocalDate? {
+        return if (source == null || source.isEmpty()) {
+            null
+        } else LocalDate.parse(source, formatter)
+    }
 }
