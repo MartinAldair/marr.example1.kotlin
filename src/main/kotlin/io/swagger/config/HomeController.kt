cd1a0dfd7a427d10476f22635ba6516597773233
DESKTOP-LER948U\Martin_Aldair <m.a.rmz.ramos@outlook.com>
2022-03-30 18:55:49 -0600

    #XI@Dev&Implemented

---
+++ src/main/kotlin/io/swagger/config/HomeController.kt
@@ -0,0 +1,21 @@
+package io.swagger.config
+
+import org.springdoc.core.Constants.SWAGGER_UI_PATH
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Controller
+import org.springframework.util.AntPathMatcher.DEFAULT_PATH_SEPARATOR
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.view.UrlBasedViewResolver.REDIRECT_URL_PREFIX
+
+@Controller
+class HomeController {
+
+    @Value(SWAGGER_UI_PATH)
+    private val swaggerUiPath: String? = null
+
+    @GetMapping(DEFAULT_PATH_SEPARATOR)
+    fun index(): String? {
+        return REDIRECT_URL_PREFIX + swaggerUiPath
+    }
+
+}
